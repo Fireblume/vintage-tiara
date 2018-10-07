bd96e4c562291cf5c7cf24602218056e4c27970c
fireblume <violeta.vns@gmail.com>
2018-10-07 21:26:36 +0200

    adding products

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DashboardService } from './dashboard.service';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Ng2ImgMaxService } from 'ng2-img-max';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,8 @@ import * as firebase from 'firebase/app';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private dashService: DashboardService, private _firebaseAuth: AngularFireAuth) {
+  constructor(private dashService: DashboardService, private _firebaseAuth: AngularFireAuth, 
+  public db: AngularFireDatabase, public ng2ImgMax: Ng2ImgMaxService) {
       
   }
 
@@ -31,9 +34,7 @@ export class DashboardComponent implements OnInit {
   selectedP:any = {};
 
   ngOnInit() {
-  console.log(this.category)
   	this.getCategory();
-    this.getProducts();
   }
 
   getCategory(){
@@ -64,12 +65,56 @@ export class DashboardComponent implements OnInit {
           });
     }
 
-  getProducts(){
-    this.products = this.dashService.getProducts();
+  getProducts(subCtgId){
+    this.dashService.getProducts(subCtgId).subscribe(
+      (res) => {
+        let container:any[] = [];
+        let result: any[] = res;
+
+        for(let i of result){
+          this.dashService.getPhoto(i.key).then(
+            (res1 => {i.value.photo = res1; container.push(i)})
+          )
+        }
+        console.log(this.products)
+        this.products = container;
+      }, 
+      (error) => {
+            this.error = error;
+            setTimeout(()=>{
+            this.error = null;
+        }, 3000);
+            console.log(error)
+          });
   }
 
   submitCatg(category){
     this.dashService.saveCategory(category);
+  }
+
+  submitProduct(product){
+    const pushId = this.db.createPushId();
+    product.photo = this.product.photo;
+    console.log(product)
+
+    this.ng2ImgMax.resizeImage(product.photo, 800, 800).subscribe(
+        result => {
+          this.dashService.pushFileToStorage(result, pushId).on(firebase.storage.TaskEvent.STATE_CHANGED,
+            (snapshot) => {
+              // in progress
+            },
+            (error) => {
+              // fail
+              console.log(error);
+            },
+            () => {
+              // success
+              this.dashService.saveProduct(product, pushId);
+            });
+          },
+         error => {
+           console.log('😢 Oh no!', error);
+        });      
   }
 
   fillFormC(category){
@@ -113,5 +158,16 @@ export class DashboardComponent implements OnInit {
   selectActiveS(key){
     this.selectedS = {};
     this.selectedS[key] = true;
+  }
+
+  selectFile(event) {
+    const file = event.target.files.item(0);
+  
+    if (file.type.match('image.*')) {
+      this.product.photo = file;
+      console.log(this.product)
+    } else {
+      alert('invalid format!');
+    }
   }
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { FirebaseApp } from 'angularfire2';
+import 'firebase/storage';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,7 +13,8 @@ import { map } from 'rxjs/operators';
 })
 export class DashboardService {
 
-  constructor(private _firebaseAuth: AngularFireAuth, public db: AngularFireDatabase) {
+  constructor(private _firebaseAuth: AngularFireAuth, public db: AngularFireDatabase,
+              public firebaseApp: FirebaseApp) {
        this._firebaseAuth.authState.subscribe((auth) => {
               this.adminUid = auth.uid;
             });
@@ -32,44 +35,11 @@ export class DashboardService {
                   }));
   }
 
-  getProducts(){
-    return [
-          {
-            'image': 'logo-try.jpg',
-            'title': 'Test test testiiici',
-            'desc': 'Test',
-            'quantity':'2',
-            'price':'200'
-          },
-          {
-            'image': 'logo-withbackground.jpg',
-            'title': 'Test tskdjklsjdlkfsjlkfjslkdj dj',
-            'desc': 'Test',
-            'quantity':'10',
-            'price':'100'
-          },
-          {
-            'image': 'logo.jpg',
-            'title': 'Test',
-            'desc': 'Test',
-            'quantity':'8',
-            'price':'3000'
-          },
-          {
-            'image': 'logo-try.jpg',
-            'title': 'Test',
-            'desc': 'TestTest',
-            'quantity':'2',
-            'price':'500'
-          },
-          {
-            'image': 'logo-withbackground.jpg',
-            'title': 'Test',
-            'desc': 'Test',
-            'quantity':'1',
-            'price':'1000'
-          }
-    ]
+  getProducts(subCtgID){
+   return this.db.list('/products/'+ this.adminUid + '/'+ subCtgID)
+                  .snapshotChanges().pipe(map(changes => {
+                    return changes.map(c => ({ key: c.payload.key, value: c.payload.val(), parentId: subCtgID }));
+                  }));
   }
 
   saveCategory(category){
@@ -102,10 +72,45 @@ export class DashboardService {
                     });
   }
 
+  saveProduct(product, pushId){
+    if(product.uidP == undefined ){
+      return this.db.list('/products/'+ this.adminUid + '/'+ product.subCtgId)
+                    .set(pushId,{
+                          title: product.prodtitle,
+                          description: product.desc,
+                          quantity: product.quantity,
+                          color: product.color,
+                          price: product.price,
+                          available: product.isAvailable
+                        });
+    }
+    else
+      return this.db.object('/products/'+ this.adminUid + '/'+ product.subCtgId +'/'+product.uidP)
+                    .update({
+                      title: product.prodtitle,
+                      description: product.desc,
+                      quantity: product.quantity,
+                      color: product.color,
+                      price: product.price,
+                      available: product.isAvailable,
+                      photo: product.photo
+                    });
+  }
+
   removeSubCateg(subCateg, catID){
     console.log("za brisanje " +catID)
     return this.db.object('/subcategories/'+ this.adminUid +'/'+ catID +'/'+subCateg.uidS)
                   .remove();
+  }
+
+  pushFileToStorage(fileUpload: File, pushId) {
+    const storageRef = this.firebaseApp.storage().ref();
+    return storageRef.child('/photos/'+ pushId).put(fileUpload);
+  }
+
+  getPhoto(productId){
+    const storageRef = this.firebaseApp.storage();
+    return storageRef.ref('/photos/'+productId).getDownloadURL();
   }
 
 }
